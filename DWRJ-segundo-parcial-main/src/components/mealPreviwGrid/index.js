c2f55d36ab8b254e6f1fd58fc6de17e47a79decb
nicholastrsu <143761613+nicholastrsu@users.noreply.github.com>
2023-10-05 18:42:53 -0500

    Add files via upload

---
+++ DWRJ-segundo-parcial-main/src/components/mealPreviwGrid/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './index.css';
+
+const MealPreviwGrid = () => {
+
+	return (
+		<div className='meal-grid'>
+		</div>
+	);
+};
+
+export default MealPreviwGrid;
